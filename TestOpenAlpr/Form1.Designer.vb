33762e34a9be73871c5b785de8e6c244207af1bf
shane-taurean <shane.taurean.git@gmail.com>
2017-02-12 19:36:16 +0530

    First Commit - Added Project Number Plate Recognition in VB.NET using OpenALPR

---
+++ TestOpenAlpr/Form1.Designer.vb
@@ -0,0 +1,248 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cmbMaxResults = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.cmbMaxVehicles = New System.Windows.Forms.ComboBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.lblCaution = New System.Windows.Forms.Label()
+        Me.lstBoxVehicles = New System.Windows.Forms.ListBox()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.StatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.StatusStrip1.SuspendLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(496, 26)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Browse..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(11, 28)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(479, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(11, 56)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(419, 285)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(140, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Path to Number Plate Image"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(482, 156)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(42, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Results"
+        '
+        'cmbMaxResults
+        '
+        Me.cmbMaxResults.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbMaxResults.FormattingEnabled = True
+        Me.cmbMaxResults.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+        Me.cmbMaxResults.Location = New System.Drawing.Point(436, 121)
+        Me.cmbMaxResults.Name = "cmbMaxResults"
+        Me.cmbMaxResults.Size = New System.Drawing.Size(135, 21)
+        Me.cmbMaxResults.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(436, 105)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(115, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Max results per vehicle"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(436, 56)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(118, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Max vehicles per image"
+        '
+        'cmbMaxVehicles
+        '
+        Me.cmbMaxVehicles.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbMaxVehicles.FormattingEnabled = True
+        Me.cmbMaxVehicles.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+        Me.cmbMaxVehicles.Location = New System.Drawing.Point(436, 72)
+        Me.cmbMaxVehicles.Name = "cmbMaxVehicles"
+        Me.cmbMaxVehicles.Size = New System.Drawing.Size(135, 21)
+        Me.cmbMaxVehicles.TabIndex = 11
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(11, 347)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 12
+        Me.Button3.Text = "Contacts DB"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'lblCaution
+        '
+        Me.lblCaution.AutoSize = True
+        Me.lblCaution.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCaution.ForeColor = System.Drawing.Color.Red
+        Me.lblCaution.Location = New System.Drawing.Point(101, 344)
+        Me.lblCaution.Name = "lblCaution"
+        Me.lblCaution.Size = New System.Drawing.Size(238, 26)
+        Me.lblCaution.TabIndex = 13
+        Me.lblCaution.Text = "Found Banned Vehicle!"
+        Me.lblCaution.Visible = False
+        '
+        'lstBoxVehicles
+        '
+        Me.lstBoxVehicles.FormattingEnabled = True
+        Me.lstBoxVehicles.Location = New System.Drawing.Point(436, 173)
+        Me.lstBoxVehicles.Name = "lstBoxVehicles"
+        Me.lstBoxVehicles.Size = New System.Drawing.Size(135, 134)
+        Me.lstBoxVehicles.TabIndex = 14
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 379)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(582, 22)
+        Me.StatusStrip1.TabIndex = 18
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'StatusLabel1
+        '
+        Me.StatusLabel1.Name = "StatusLabel1"
+        Me.StatusLabel1.Size = New System.Drawing.Size(0, 17)
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(466, 347)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 19
+        Me.Button2.Text = "Save Image"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox3.Location = New System.Drawing.Point(436, 311)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(135, 30)
+        Me.PictureBox3.TabIndex = 21
+        Me.PictureBox3.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(582, 401)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.lstBoxVehicles)
+        Me.Controls.Add(Me.lblCaution)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.cmbMaxVehicles)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.cmbMaxResults)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "OpenAlpr Test _ Shane (x86)"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents cmbMaxResults As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents cmbMaxVehicles As System.Windows.Forms.ComboBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents lblCaution As System.Windows.Forms.Label
+    Friend WithEvents lstBoxVehicles As System.Windows.Forms.ListBox
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents StatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ TestOpenAlpr/Form1.vb
@@ -0,0 +1,252 @@
+﻿Imports openalprnet
+Imports System.IO
+Imports System.Drawing
+Imports System.Drawing.Imaging
+Imports System.Linq
+
+Public Class Form1
+    Dim vehicleResults As List(Of VehicleResult)
+    Dim images As List(Of Image)
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        OpenFileDialog1.InitialDirectory = My.Application.Info.DirectoryPath
+        cmbMaxResults.SelectedIndex = 0
+        cmbMaxVehicles.SelectedIndex = 0
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        StatusLabel1.Text = ""
+        OpenFileDialog1.Multiselect = False
+        OpenFileDialog1.Filter = "Image Files |*.JPG; *.JPEG; *.PNG; *.BMP|JPEG Image |*.JPG; *.JPEG|PNG Image |*.PNG|Bitmap |*.BMP"
+        OpenFileDialog1.Title = "Select an Image"
+        OpenFileDialog1.FileName = ""
+
+        If OpenFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            lblCaution.Visible = False
+            TextBox1.Text = OpenFileDialog1.FileName
+            PictureBox1.Image = Image.FromFile(TextBox1.Text)
+            processImage()
+        End If
+    End Sub
+
+    Private Function recognizeNumber(ByVal path As String) As Boolean
+        Dim foundBanned As Boolean = False
+        Try
+            Dim confPath As String = My.Application.Info.DirectoryPath & "\openalpr.conf"
+            Dim runtimePath As String = My.Application.Info.DirectoryPath & "\runtime_data\"
+
+            Dim alpr As AlprNet = New AlprNet("eu", confPath, runtimePath)
+
+            If Not alpr.IsLoaded Then
+                Throw New System.Exception("Error loading openalpr library")
+            End If
+
+            'alpr.DefaultRegion = "eu"
+            alpr.DetectRegion = True
+            alpr.TopN = Val(cmbMaxResults.Text)
+
+            Dim results As AlprResultsNet = alpr.Recognize(path)
+
+            If Not IsNothing(images) Then
+                images = Nothing
+            End If
+
+            If Not IsNothing(vehicleResults) Then
+                vehicleResults = Nothing
+            End If
+
+            images = New List(Of Image)(results.Plates.Count())
+            vehicleResults = New List(Of VehicleResult)
+
+            Dim count As Int16 = 1
+            Dim listIndex As Int16 = 0
+
+            If results.Plates.Count > 0 Then
+                For Each result As AlprPlateResultNet In results.Plates
+                    If count > Val(cmbMaxVehicles.Text) Then
+                        Exit For
+                    End If
+
+                    Dim rect As Rectangle = boundingRectangle(result.PlatePoints)
+                    Dim img As Image = Image.FromFile(path)
+                    Dim cropped As Image = cropImage(img, rect)
+                    cropped = resizeImage(cropped)
+                    images.Add(cropped)
+
+                    For Each plate As AlprPlateNet In result.TopNPlates
+                        lstBoxVehicles.Items.Add(plate.Characters)
+                        'lstBoxVehicles.SelectedIndex = 0
+
+                        If Not foundBanned And vehContact.isBarred(plate.Characters) Then
+                            foundBanned = True
+                        End If
+
+                        vehicleResults.Add(New VehicleResult(plate.Characters, listIndex, count - 1))
+
+                        listIndex += 1
+                    Next
+
+                    count += 1
+                Next
+
+                If foundBanned Then
+                    lblCaution.Visible = True
+                End If
+
+                Return True
+            Else
+                Return False
+            End If
+        Catch ex As Exception
+            'MessageBox.Show(ex.Message)
+        End Try
+
+        Return False
+    End Function
+
+    Private Sub processImage()
+        lstBoxVehicles.Items.Clear()
+        lstBoxVehicles.Text = ""
+
+        If File.Exists(TextBox1.Text) Then
+            lblCaution.Visible = False
+            lstBoxVehicles.Items.Clear()
+            lstBoxVehicles.Text = ""
+            PictureBox3.Image = Nothing
+            StatusLabel1.Text = "Processing"
+
+            If Not recognizeNumber(TextBox1.Text) Then
+                StatusLabel1.Text = "-- FAILED! --"
+                Button2.Enabled = False
+                lstBoxVehicles.Items.Clear()
+                lstBoxVehicles.Text = ""
+                PictureBox3.Image = Nothing
+            Else
+                StatusLabel1.Text = "Done"
+                Button2.Enabled = True
+                lstBoxVehicles.SelectedIndex = 0
+            End If
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Form2.ShowDialog()
+        StatusLabel1.Text = ""
+    End Sub
+
+    Private Sub cmb_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbMaxVehicles.SelectedIndexChanged, cmbMaxResults.SelectedIndexChanged
+        processImage()
+    End Sub
+
+    Public Function boundingRectangle(ByVal points As List(Of Point)) As Rectangle
+        Dim minX As Integer = points.Min(Function(p) p.X)
+        Dim minY As Integer = points.Min(Function(p) p.Y)
+        Dim maxX As Integer = points.Max(Function(p) p.X)
+        Dim maxY As Integer = points.Max(Function(p) p.Y)
+        Return New Rectangle(New Point(minX, minY), New Size(maxX - minX, maxY - minY))
+    End Function
+
+    Private Function cropImage(ByVal img As Image, ByVal cropArea As Rectangle) As Image
+        Dim bmpImage As Bitmap = New Bitmap(img)
+        Return bmpImage.Clone(cropArea, bmpImage.PixelFormat)
+    End Function
+
+    Public Function combineImages(ByVal images As List(Of Image)) As Bitmap
+        'read all images into memory
+        Dim finalImage As Bitmap = Nothing
+
+        Try
+            Dim width As Integer = 0
+            Dim height As Integer = 0
+
+            For Each bmp As Image In images
+                width += bmp.Width
+                If bmp.Height > height Then
+                    height = bmp.Height
+                End If
+            Next
+
+            'create a bitmap to hold the combined image
+            finalImage = New Bitmap(width, height)
+
+            'get a graphics object from the image so we can draw on it
+            Using g As Graphics = Graphics.FromImage(finalImage)
+                'set background color
+                g.Clear(Color.Black)
+
+                'go through each image and draw it on the final image
+                Dim offset As Integer = 0
+
+                For Each Image As Bitmap In images
+                    g.DrawImage(Image, New Rectangle(offset, 0, Image.Width, Image.Height))
+                    offset += Image.Width
+                Next
+
+            End Using
+
+            Return finalImage
+        Catch ex As Exception
+            If Not IsNothing(finalImage) Then finalImage.Dispose()
+
+            Throw ex
+        Finally
+            For Each image As Image In images
+                image.Dispose()
+            Next
+        End Try
+    End Function
+
+    Public Function resizeImage(ByVal image As Image) As Bitmap
+        Dim BOXHEIGHT As Int16 = 30
+        Dim BOXWIDTH As Int16 = 135
+
+        Dim scaleHeight As Double = CDbl(BOXHEIGHT) / CDbl(image.Height)
+        Dim scaleWidth As Double = CDbl(BOXWIDTH) / CDbl(image.Width)
+
+        Dim scale As Double = Math.Min(scaleHeight, scaleWidth)
+
+        Dim resizedImage As Bitmap = New Bitmap(image, CInt(image.Width * scale), CInt(image.Height * scale))
+
+        Return resizedImage
+    End Function
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If Not IsNothing(PictureBox3.Image) Then
+            Dim fileName As String = lstBoxVehicles.SelectedItem.ToString
+            Dim fileExtenstion As String = ""
+            Dim fileType As ImageFormat = Nothing
+            SaveFileDialog1.Filter = "Image Files |*.JPG; *.JPEG; *.PNG; *.BMP|JPEG Image |*.JPG; *.JPEG|PNG Image |*.PNG|Bitmap |*.BMP"
+            SaveFileDialog1.Title = "Save Image"
+            SaveFileDialog1.DefaultExt = "jpg"
+            SaveFileDialog1.FileName = fileName
+
+            If SaveFileDialog1.ShowDialog = DialogResult.OK Then
+                fileName = SaveFileDialog1.FileName
+
+                If fileName <> "" Then
+                    fileExtenstion = Path.GetExtension(fileName).ToLower
+                    If fileExtenstion = ".jpg" Or fileExtenstion = ".jpeg" Then
+                        fileType = ImageFormat.Jpeg
+                    ElseIf fileExtenstion = ".bmp" Then
+                        fileType = ImageFormat.Bmp
+                    ElseIf fileExtenstion = ".png" Then
+                        fileType = ImageFormat.Png
+                    End If
+
+                    If Not IsNothing(fileType) Then
+                        PictureBox3.Image.Save(fileName, fileType)
+                    End If
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub lstBoxVehicles_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lstBoxVehicles.SelectedIndexChanged
+        If vehicleResults.Any Then
+            Dim vehResult As VehicleResult = vehicleResults.Find(Function(x) x.listID = lstBoxVehicles.SelectedIndex)
+            PictureBox3.Image = images(vehResult.imageID)
+        End If
+    End Sub
+
+
+End Class
